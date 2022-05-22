40e272d2805d73d61a5f41f63cf21cc78718a854
Yee Choong <yeechoong97@gmail.com>
2022-05-22 12:05:57 +0800

    Created HeaderTabs

---
+++ components/HeaderTab.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+const HeaderButton = ({ title, activeTab, setActiveTab }) => {
+    return (
+        <View>
+            <TouchableOpacity style={[styles.headerButton, { backgroundColor: activeTab === title ? 'black' : 'white' }]} onPress={() => setActiveTab(title)}>
+                <Text style={[styles.buttonText, { color: activeTab === title ? 'white' : 'black' }]}>{title}</Text>
+            </TouchableOpacity>
+        </View >
+    )
+}
+
+
+const HeaderTab = () => {
+
+    const [activeTab, setActiveTab] = useState("Delivery")
+
+    return (
+        <View style={styles.headerTab}>
+            {/* Header Button 1 */}
+            <HeaderButton title={"Delivery"} activeTab={activeTab} setActiveTab={setActiveTab} />
+            {/* Header Button 2 */}
+            <HeaderButton title={"Pickup"} activeTab={activeTab} setActiveTab={setActiveTab} />
+        </View>
+    )
+}
+
+export default HeaderTab
+
+const styles = StyleSheet.create({
+    headerTab: {
+        flexDirection: "row",
+        alignSelf: 'center'
+    },
+    headerButton: {
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 30,
+    },
+    buttonText: {
+        fontWeight: "900",
+        fontSize: 15
+    }
+})
+
---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import HeaderTab from '../components/HeaderTab'
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <HeaderTab />
+        </SafeAreaView>
+    )
+}
+
+export default Home
