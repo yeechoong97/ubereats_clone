54b97377fdb1b5a9283a6bc93e036371efdfd7d5
Yee Choong <yeechoong97@gmail.com>
2022-06-08 21:30:33 +0800

    Completed Login Component

--- screens/Login.js
+++ screens/Login.js
@@ -1,5 +1,6 @@
-import { View, Text, StyleSheet, Image, TextInput, Button, KeyboardAvoidingView } from 'react-native'
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
 import React, { useState } from 'react';
+
 
 
 const Login = () => {
@@ -7,16 +8,18 @@ const Login = () => {
     const [focus, setFocus] = useState(null);
 
     return (
-        <View style={styles.loginContainer}>
+        <KeyboardAvoidingView style={styles.loginContainer} behavior={Platform.OS === "ios" ? "padding" : "height"}>
             <View style={styles.formContainer}>
                 <Image source={require('../assets/images/ubereats.png')} style={styles.logoImage} />
-                <TextInput style={focus === 'email' ? styles.focusTextInput : styles.textInput} placeholder='Email' onChangeText={() => { }} onFocus={() => setFocus('email')} />
+                <TextInput style={focus === 'email' ? styles.focusTextInput : styles.textInput} placeholder='Email' onChangeText={() => { }} onFocus={() => setFocus('email')} keyboardType="email-address" />
                 <TextInput style={focus === 'password' ? styles.focusTextInput : styles.textInput} placeholder='Password' onChangeText={() => { }} secureTextEntry={true} onFocus={() => setFocus('password')} />
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.loginButton} onPress={() => { }} activeOpacity={0.7}>
+                        <Text style={styles.loginText}>Login</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <KeyboardAvoidingView style={styles.buttonContainer}>
-                <Button title='Login' />
-            </KeyboardAvoidingView>
-        </View>
+        </KeyboardAvoidingView>
     )
 }
 
@@ -26,16 +29,18 @@ const styles = StyleSheet.create({
     loginContainer: {
         flex: 1,
         justifyContent: 'space-between',
-        marginVertical: '20%',
         alignItems: 'center',
     },
     logoImage: {
         width: 125,
         height: 125,
+        marginVertical: 50,
     },
     formContainer: {
-        width: '80%',
+        width: '100%',
         marginVertical: 20,
+        alignItems: 'center',
+        padding: 10,
     },
     textInput: {
         marginVertical: 10,
@@ -44,6 +49,7 @@ const styles = StyleSheet.create({
         padding: 10,
         borderColor: '#a3a3a3',
         fontSize: 16,
+        width: '80%',
     },
     focusTextInput: {
         marginVertical: 10,
@@ -52,11 +58,22 @@ const styles = StyleSheet.create({
         padding: 10,
         borderColor: '#4b4b4b',
         fontSize: 16,
+        width: '80%',
     },
     buttonContainer: {
-        alignSelf: 'flex-end',
-        flexDirection: 'column',
-        backgroundColor: 'red',
-        width: 200,
+        width: '40%',
+        marginVertical: 10,
+    },
+    loginButton: {
+        width: '100%',
+        backgroundColor: '#283346',
+        borderRadius: 12,
+        padding: 15,
+        alignItems: 'center',
+    },
+    loginText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: '700',
     }
 })
